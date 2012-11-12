ddbaccfcf67f3a98f22e9bf0897d2fe73bc9d076
Iain Ballard <iain.ballard@7digital.com>
2012-11-12 17:54:29 +0000

    More terminals tests

--- Phantom.Unit.Tests/TerminalParsers/RegularExpressionTests.cs
+++ Phantom.Unit.Tests/TerminalParsers/RegularExpressionTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Phantom.Parsers;
 using Phantom.Parsers.Terminals;
 using Phantom.Scanners;
@@ -12,22 +8,70 @@ namespace Phantom.Unit.Tests.TerminalParsers
 	[TestFixture]
 	public class RegularExpressionTests
 	{
-		IScanner scanner;
-		IParser subject;
-		const string Input = "This is my input";
-
-		[SetUp]
-		public void a_string_scanner_with_some_text ()
+		[Test]
+		[TestCase(@"\s\w+\s", " word another, third", " word ")]
+		[TestCase(@"\w+", "word another, third", "word")]
+		[TestCase(@".+", "word another, third", "word another, third")]
+		[TestCase(@"[^,]*", "word another, third", "word another")]
+		[TestCase(@".*", "word\non a new line", "word")]
+		public void matching_regexes_result_in_matching_parses_and_contain_the_match_and_advance_the_scanner
+			(string regex, string inputString, string match)
 		{
-			scanner = new ScanStrings(Input);
-			subject = new RegularExpression(@"\s\w+\s");
+			IParser subject = new RegularExpression(regex);
+			var scanner = new ScanStrings(inputString);
+			var result = subject.TryMatch(scanner);
+
+			Assert.That(result.Success, Is.True, "Match result");
+			Assert.That(result.Value, Is.EqualTo(match), "Parser match string");
+			Assert.That(scanner.Offset, Is.EqualTo(match.Length), "Scanner offset");
+		}
+		
+		[Test]
+		[TestCase(@"^.*$", "word \n on a new line", false)]
+		[TestCase(@"^.*$", "word    on a new line", true)]
+		public void regexes_are_single_line_setting_by_default
+			(string regex, string inputString, bool isMatch)
+		{
+			IParser subject = new RegularExpression(regex);
+			var scanner = new ScanStrings(inputString);
+			var result = subject.TryMatch(scanner);
+
+			Assert.That(result.Success, Is.EqualTo(isMatch), "Match result");
 		}
 
 		[Test]
-		public void matches_regexes ()
+		public void regexes_whose_match_does_not_start_at_the_current_scanner_position_are_failures ()
 		{
-			// TODO: write me!
-			Assert.Inconclusive();
+			IParser subject = new RegularExpression(@"\w+");
+			var scanner = new ScanStrings("      some words");
+
+			var result = subject.TryMatch(scanner);
+
+			Assert.That(result.Success, Is.False);
+		}
+		
+		[Test] // this is a gotcha of PCRE as implemented in .Net
+		public void regex_dot_matches_carriage_return_but_not_line_feed ()
+		{
+			IParser subject = new RegularExpression(@".*");
+			var scanner = new ScanStrings("line one\r\nline two");
+			var result = subject.TryMatch(scanner);
+
+			Assert.That(result.Value, Contains.Substring("\r"));
+		}
+
+
+		[Test]
+		[TestCase(@"\s\w+\s", "word another, third")]
+		public void non_matching_regexes_result_in_failing_parses_and_do_not_advance_the_scanner
+			(string regex, string inputString)
+		{
+			IParser subject = new RegularExpression(regex);
+			var scanner = new ScanStrings(inputString);
+			var result = subject.TryMatch(scanner);
+
+			Assert.That(result.Success, Is.False, "Match result");
+			Assert.That(scanner.Offset, Is.EqualTo(0), "Scanner offset");
 		}
 	}
 }
---
+++ Phantom.Unit.Tests/TerminalParsers/RegularExpressionsSettingsTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Phantom.Unit.Tests.TerminalParsers
+{
+	[TestFixture]
+	public class RegularExpressionsSettingsTests
+	{
+
+		[Test]
+		public void regular_expression_parser_can_have_Regex_options_set ()
+		{
+			Assert.Inconclusive();
+		}
+	}
+}
