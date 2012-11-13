62c98c05b5b4ad03970a78aab6417f3c298dd30c
Iain Ballard <iain.ballard@7digital.com>
2012-11-13 12:48:03 +0000

    regex options

--- Phantom.Unit.Tests/TerminalParsers/RegularExpressionsSettingsTests.cs
+++ Phantom.Unit.Tests/TerminalParsers/RegularExpressionsSettingsTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Text.RegularExpressions;
+using NUnit.Framework;
+using Phantom.Parsers.Terminals;
+using Phantom.Scanners;
 
 namespace Phantom.Unit.Tests.TerminalParsers
 {
@@ -7,9 +10,28 @@ namespace Phantom.Unit.Tests.TerminalParsers
 	{
 
 		[Test]
-		public void regular_expression_parser_can_have_Regex_options_set ()
+		[TestCase("one\ntwo", "^.*$", RegexOptions.Multiline, true, "one")]
+
+		[TestCase("one\ntwo", @"^\w*\s\w*$", RegexOptions.None, true, "one\ntwo")]
+		[TestCase("one\ntwo", @"^.*$", RegexOptions.None, false, "")]
+		
+		[TestCase("hello", "HELLO", RegexOptions.IgnoreCase, true, "hello")]
+
+		[TestCase("hello", "h e l l o", RegexOptions.IgnorePatternWhitespace, true, "hello")]
+		
+		[TestCase("mind\nthe\ngap", @"^.*$", RegexOptions.Singleline, true, "mind\nthe\ngap")]
+		[TestCase("one\ntwo", @".+", RegexOptions.Singleline, true, "one\ntwo")]
+		[TestCase("one\ntwo", @"^\w$", RegexOptions.Singleline, false, "")]
+
+		public void regular_expression_parser_can_have_Regex_options_set
+			(string inputString, string pattern, RegexOptions options, bool success, string match)
 		{
-			Assert.Inconclusive();
+			var subject = new RegularExpression(pattern, options);
+			var scanner = new ScanStrings(inputString);
+			var result = subject.TryMatch(scanner);
+
+			Assert.That(result.Success, Is.EqualTo(success));
+			if (success) Assert.That(result.Value, Is.EqualTo(match));
 		}
 	}
 }
