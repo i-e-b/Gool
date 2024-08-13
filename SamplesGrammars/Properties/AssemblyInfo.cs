c32ffa69db38bcac4875545ac574785b63191b63
Iain Ballard <iain.ballard@gmail.com>
2024-08-13 18:09:49 +0100

    clean-up

--- SamplesGrammars/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-[assembly: AssemblyTitle("SamplesGrammars")]
-[assembly: AssemblyProduct("SamplesGrammars")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
